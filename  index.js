4a86e28668324fe60121cae858e62e36f9ab4411
SplashyCoder <pachecomorahectordavid@gmail.com>
2023-05-16 00:07:44 -0500

    Html basic, js with a prove of poo and random for start with the project logic

---  index.js
+++  index.js
@@ -7,19 +7,24 @@
     
         sumar() {
             const sumar1 = () => {
-                this.resultado = this.first + this.second;
-                return this.resultado;
-        }
-        return sumar1;
+                this.resultado = Number(this.first) + Number(this.second);
+                alert(this.resultado);
+
+                let numero = Math.random() * 100
+                numero = Math.round(numero)
+
+                document.write(numero)
+            }
+            return sumar1;
         }
     }
     
         const suma =()=>{
             first = prompt('1')
             second = prompt('2')
-            
-            const plus = new Calculadora(first, second)
-            plus.sumar();
+            const plus = new Calculadora(first,second)
+            const sumar1 = plus.sumar();
+            sumar1();
         }
-    suma()
-    
+
+        suma();
